4a780c1878deec52d58f99a176f29c1c2aaa4e8a
Radek <radekkozaczuk1@gmail.com>
2022-09-02 13:41:36 +0200

    added comments for better readability

--- Assets/Scripts/Presentation/PresentationReferenceHolder.cs
+++ Assets/Scripts/Presentation/PresentationReferenceHolder.cs
@@ -4,6 +4,11 @@ using Shared.DependencyInjector.Attributes;
 
 namespace Presentation
 {
+    /// <summary>
+    /// Reference holders allow use to us injected classes in static classes.
+    /// We cannot use <see cref="InjectAttribute"/> in static classes as static objects are not controlled via <see cref="Shared.DependencyInjector"/>.
+    /// Do not confuse reference holders with scene reference holders are these are two different things.
+    /// </summary>
     [UsedImplicitly]
     class PresentationReferenceHolder
     {
--- Assets/Scripts/UI/UIReferenceHolder.cs
+++ Assets/Scripts/UI/UIReferenceHolder.cs
@@ -4,6 +4,11 @@ using UI.Controllers;
 
 namespace UI
 {
+    /// <summary>
+    /// Reference holders allow use to use injected classes in static classes.
+    /// We cannot use <see cref="InjectAttribute"/> in static classes as static objects are not controlled via <see cref="Shared.DependencyInjector"/>.
+    /// Do not confuse reference holders with scene reference holders are these are two different things.
+    /// </summary>
     [UsedImplicitly]
     class UIReferenceHolder
     {
