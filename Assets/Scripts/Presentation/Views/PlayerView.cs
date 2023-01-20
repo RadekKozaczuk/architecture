afe3d03935f9fb12416f4a5274bb9f65637f6464
Radek <radekkozaczuk1@gmail.com>
2023-01-20 11:50:49 +0700

    cleanup

--- Assets/Scripts/Presentation/Views/PlayerView.cs
+++ Assets/Scripts/Presentation/Views/PlayerView.cs
@@ -16,10 +16,7 @@ namespace Presentation.Views
         {
             Assert.True(v.sqrMagnitude < Vector3.one.sqrMagnitude, "Vector v must be normalized.");
 
-            // dealing with additional speed gained by using diagonal movement
             Vector3 movement = new Vector3(v.x, 0, v.y) * _playerConfig.PlayerSpeed;
-
-            Debug.Log(v.x + "  " + v.y);
             _characterController.SimpleMove(movement * Time.deltaTime);
         }
     }
