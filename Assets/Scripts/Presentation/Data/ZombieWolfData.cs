24221789495be524dabe8563fad0cb422164851c
Radek <radekkozaczuk1@gmail.com>
2023-03-30 22:12:24 +0200

    test class

---
+++ Assets/Scripts/Presentation/Data/ZombieWolfData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Presentation.Data
+{
+    [CreateAssetMenu(fileName = "EnemyData", menuName = "Data/ZombieWolfData")]
+    class ZombieWolfData : ScriptableObject
+    {
+        [Tooltip("Movement speed of the enemy")]
+        [SerializeField]
+        [Range(0.1f, 5f)]
+        internal float BasicSpeed;
+    }
+}
