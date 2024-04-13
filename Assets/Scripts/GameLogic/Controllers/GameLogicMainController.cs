4f9fd0b763900a2fb29d4d8c927f2cd39f45eb71
RadekKozaczuk <radekkozaczuk1@gmail.com>
2024-04-13 08:52:00 +0200

    better comment syntax

--- Assets/Scripts/GameLogic/Controllers/GameLogicMainController.cs
+++ Assets/Scripts/GameLogic/Controllers/GameLogicMainController.cs
@@ -8,12 +8,12 @@ namespace GameLogic.Controllers
 {
     /// <summary>
     /// Main controller serves 3 distinct roles:
-    /// 1) It allows you to order the signal execution order. So instead of reacting separately in two different controllers you can react in main controller
-    /// and call adequate methods.
-    /// 2) Serves as a 'default' controller. When you don't know where to put some logic or the logic is too small for its own controller you can put it into
-    /// the main controller.
-    /// 3) Reduces the size of the view model. We could move all (late/fixed)update calls to view model but as the application grows it would lead to view
-    /// model doing too much stuff.
+    /// 1) It allows you to order signal execution order. Instead of reacting on many signals in many different controllers,
+    /// you can have one signal react on it here and call necessary controllers in the order of your liking.
+    /// 2) Serves as a 'default' controller. When you don't know where to put some logic or the logic is too small for its own controller
+    /// you can put it into the main controller.
+    /// 3) Reduces the size of the view model. We could move all (late/fixed)update calls to view model but with the application grows
+    /// it would lead to view model doing too much stuff. Same with signals - we don't react on them in viewmodels.
     /// For better code readability all controllers meant to interact with this controller should implement
     /// <see cref="ICustomLateUpdate" /> interface.
     /// </summary>
