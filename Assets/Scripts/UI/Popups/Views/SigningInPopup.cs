55d0bb84690b194ebb92e85f618663052f35be00
RadekKozaczuk <radekkozaczuk1@gmail.com>
2024-08-08 18:13:38 +0200

    cleanup

--- Assets/Scripts/UI/Popups/Views/SigningInPopup.cs
+++ Assets/Scripts/UI/Popups/Views/SigningInPopup.cs
@@ -1,5 +1,4 @@
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-using System.Collections.Generic;
 using Core.Enums;
 using GameLogic.ViewModels;
 using Unity.Services.Authentication;
@@ -48,7 +47,7 @@ namespace UI.Popups.Views
             GameLogicViewModel.LoginVoiceChat(HasUserJoinedLobbies);
         }
 
-        async void HasUserJoinedLobbies()
+        static async void HasUserJoinedLobbies()
         {
             string? lobbyId = await GameLogicViewModel.GetJoinedLobbyAsync();
 
