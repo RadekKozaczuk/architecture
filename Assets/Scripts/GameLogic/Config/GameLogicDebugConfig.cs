a2ee366b4a94ac47018901a232835547ba7f945d
Radek <106958102+RadekKozaczuk@users.noreply.github.com>
2024-09-21 12:28:03 +0200

    Unity upgrade + cleanup (#52)

--- Assets/Scripts/GameLogic/Config/GameLogicDebugConfig.cs
+++
@@ -1,8 +0,0 @@
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-using UnityEngine;
-
-namespace GameLogic.Config
-{
-    [CreateAssetMenu(fileName = "GameLogicDebugConfig", menuName = "Config/GameLogic/GameLogicDebugConfig")]
-    class GameLogicDebugConfig : ScriptableObject { }
-}
